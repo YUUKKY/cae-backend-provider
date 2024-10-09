596be6e11de3c84236fdf22d94a396857a10026f
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2024-10-09 17:54:08 +0800

    init

---
+++ src/main/java/com/demo/NacosProviderApplication.java
@@ -0,0 +1,44 @@
+package com.demo;
+
+import com.demo.vo.UserDataVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author xiaojing
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@CrossOrigin
+public class NacosProviderApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+    @RestController
+    public class TestController {
+
+        private final RestTemplate restTemplate;
+
+        @Autowired
+        public TestController(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
+
+        @RequestMapping(value = "/query", method = RequestMethod.GET)
+        public UserDataVo getData(@RequestParam(value = "user_name") String userName) {
+            return restTemplate.getForObject("http://service-consumer/v1/query?user_name=root", UserDataVo.class);
+        }
+    }
+}
